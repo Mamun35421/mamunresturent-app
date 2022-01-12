3288d2d8dcb14e99f4a4c2f388a934cc5875d532
Mamun35421 <mamun35-421@diu.edu.bd>
2022-01-12 17:47:45 +0600

    card

--- src/Components/Order/orderForm.js
+++ src/Components/Order/orderForm.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Form from '../../layouts/Form'
 import { Grid } from '@material-ui/core'
 import Input from '../../controls/input'
+import Select from '../../controls/select'
+
+
+const pMethods = [
+    {id:'none', title: 'Select'},
+    {id:'Cash', title: 'Cash'},
+    {id:'Card', title: 'Card'},
+]
 
 
 
@@ -16,8 +24,24 @@ export default function orderForm() {
                     name = "Order Number"
                     />
 
+                    <Select 
+                    label="Customer"
+                    name="CustomerId"
+                    options={[
+                        {id:0,title : 'selecter'},
+                        {id:1,title : 'Customer 1'},
+                        {id:2,title : 'Customer 2'},
+                        {id:3,title : 'Customer 3'},
+                        {id:4,title : 'Customer 4'}
+                    ]}
+               />
                 </Grid>
                 <Grid item xs={6}>
+                <Select 
+                    label="Payment Method"
+                    name="PmETHOD"
+                    options={pMethods}
+            />
                 <Input
                     disabled  
                     label="Grand Total"
